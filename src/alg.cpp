04e81999da30df0ecff7c4a9dcc47328ab94772d
BaRyan1 <76247481+BaRyan1@users.noreply.github.com>
2021-03-23 17:31:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,17 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    return (a == 0) ? b: gcd(b % a, a);
+}
+ 
+int main() {
+    int n, m = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        m = gcd(m, a);
+    }
+    cout << m;
+    return 0;
 }
