aee07160b6aa306a865ede65105eeaf3e0075b23
BaRyan1 <76247481+BaRyan1@users.noreply.github.com>
2021-03-29 20:46:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-  if (a == b)
-    return a;
-  if (a > b)
-    return gcd (a-b, b);
-  return gcd (a, b-a);
-}
+                int res;
+        while ((b != 0) && (a != 0)) {
+                if (a > b)
+        a -= b;
+                else
+        b -= a;
+        }
+        res = a + b;
+                return res;
+        }
